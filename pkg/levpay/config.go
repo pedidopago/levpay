97090c860b54f77b9cda5637a55039b59499037d
Gustavo Lima <gustavolimash@gmail.com>
2019-09-24 16:18:07 -0300

    test new function

--- pkg/levpay/config.go
+++ pkg/levpay/config.go
@@ -1,8 +1,10 @@
 package levpay
 
 import (
+	"bytes"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 
 	"go.uber.org/zap"
@@ -18,16 +20,46 @@ type Config struct {
 }
 
 func (c *Config) Do(method, urlpart string, body io.Reader) (*http.Response, error) {
-	request, err := http.NewRequest(method, "https://homolog.levpay.com/publicapi"+urlpart, body)
+	req, err := http.NewRequest(method, "https://homolog.levpay.com/publicapi"+urlpart, body)
 	if err != nil {
 		return nil, err
 	}
 	fmt.Println("Passou")
 
-	request.Header.Set("Content-type", "application/json")
-	request.Header.Set("Accept", "application/json")
-	request.SetBasicAuth(c.ApiKey, c.SecretKey)
+	req.Header.Set("Content-type", "application/json")
+	req.Header.Set("Accept", "application/json")
+	req.SetBasicAuth(c.ApiKey, c.SecretKey)
+
+	if c.Trace {
+		buf := new(bytes.Buffer)
+		buf.WriteString(fmt.Sprintf("%v %v %v\n", req.Method, req.URL, req.Proto))
+		buf.WriteString(fmt.Sprintf("Host: %v\n", req.Host))
+		// headers
+		for name, headers := range req.Header {
+			for _, v := range headers {
+				buf.WriteString(fmt.Sprintf("%v: %v\n", name, v))
+			}
+		}
+
+		if req.Body != nil {
+			buf.WriteString("\n")
+			buf2 := new(bytes.Buffer)
+			io.Copy(buf2, req.Body)
+			bbytes := buf2.Bytes()
+			req.Body.Close()
+			buf2.Reset()
+			buf2.Write(bbytes)
+			req.Body = ioutil.NopCloser(buf2)
+			buf.Write(bbytes)
+			buf.WriteRune('\n')
+		}
+		fmt.Println(buf.String())
+	}
+	//
+	if c.Client == nil {
+		return http.DefaultClient.Do(req)
+	}
 
 	fmt.Println("passou 2")
-	return c.Client.Do(request)
+	return c.Client.Do(req)
 }
