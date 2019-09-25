270c3f4c8ea48ad9d3fb8bd48bdbe7e036b79232
Gustavo Lima <gustavolimash@gmail.com>
2019-09-25 13:55:03 -0300

    create a function to payment method

--- pkg/levpay/transfer/transfer.go
+++ pkg/levpay/transfer/transfer.go
@@ -65,3 +65,30 @@ func (api *API) GetLevpayAvailableAccounts(domainID int) ([]levpay.BankAccount, 
 
 	return accounts, nil
 }
+
+// CreateLevpayPayment create a new payment at Levpay and return a LevpayOrder object
+// containing order details and the payment URL (if available)
+func (api *API) CreateLevpayPayment(domainID int, orderData levpay.LevpayOrderData) (levpay.LevpayOrder, error) {
+	var order levpay.LevpayOrder
+
+	response, err := api.Config.Do(http.MethodPost, "/instance/levpay/checkout/", nil)
+	if err != nil {
+		fmt.Println("[LEVPAY] CreateLevpayPayment e1", domainID, err.Error())
+		return order, err
+	}
+	defer response.Body.Close()
+
+	responseBody, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("[LEVPAY] CreateLevpayPayment e2", domainID, err.Error())
+		return order, err
+	}
+
+	err = json.Unmarshal(responseBody, &order)
+	if err != nil {
+		fmt.Println("[LEVPAY] CreateLevpayPayment e3", domainID, err.Error(), string(responseBody))
+		return order, err
+	}
+
+	return order, nil
+}
