d41418fff025c583bad283cd983e0d20d8f7d81e
Gustavo Lima <gustavolimash@gmail.com>
2019-09-25 09:12:47 -0300

    function to generate token

--- pkg/levpay/config.go
+++ pkg/levpay/config.go
@@ -2,6 +2,7 @@ package levpay
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -9,6 +10,8 @@ import (
 
 	"go.uber.org/zap"
 )
+
+const baseURL string = "https://homolog.levpay.com/publicapi"
 
 // Config define the struct to configurated Levpay
 type Config struct {
@@ -19,37 +22,48 @@ type Config struct {
 	Client    *http.Client
 }
 
+// Do is a function to execute every api of levpay
 func (c *Config) Do(method, urlpart string, body io.Reader) (*http.Response, error) {
-	req, err := http.NewRequest(method, "https://homolog.levpay.com/publicapi"+urlpart, body)
+
+	// generate token
+	token, err := c.getLevpayAuthenticationToken()
+	if err != nil {
+		fmt.Println("[LEVPAY] GetLevpayAvailableAccounts e1", err.Error())
+		return nil, err
+	}
+
+	// execute main api
+	request, err := http.NewRequest(method, baseURL+urlpart, body)
 	if err != nil {
 		return nil, err
 	}
-	fmt.Println("Passou - ", c)
 
-	req.Header.Set("Content-type", "application/json")
-	req.Header.Set("Accept", "application/json")
-	req.SetBasicAuth(c.ApiKey, c.SecretKey)
+	bearer := "Bearer " + token.Token
+	request.Header.Set("Authorization", bearer)
+	request.Header.Set("Content-type", "application/json")
+	request.Header.Set("Accept", "application/json")
+	request.SetBasicAuth(c.ApiKey, c.SecretKey)
 
 	if c.Trace {
 		buf := new(bytes.Buffer)
-		buf.WriteString(fmt.Sprintf("%v %v %v\n", req.Method, req.URL, req.Proto))
-		buf.WriteString(fmt.Sprintf("Host: %v\n", req.Host))
+		buf.WriteString(fmt.Sprintf("%v %v %v\n", request.Method, request.URL, request.Proto))
+		buf.WriteString(fmt.Sprintf("Host: %v\n", request.Host))
 		// headers
-		for name, headers := range req.Header {
+		for name, headers := range request.Header {
 			for _, v := range headers {
 				buf.WriteString(fmt.Sprintf("%v: %v\n", name, v))
 			}
 		}
 
-		if req.Body != nil {
+		if request.Body != nil {
 			buf.WriteString("\n")
 			buf2 := new(bytes.Buffer)
-			io.Copy(buf2, req.Body)
+			io.Copy(buf2, request.Body)
 			bbytes := buf2.Bytes()
-			req.Body.Close()
+			request.Body.Close()
 			buf2.Reset()
 			buf2.Write(bbytes)
-			req.Body = ioutil.NopCloser(buf2)
+			request.Body = ioutil.NopCloser(buf2)
 			buf.Write(bbytes)
 			buf.WriteRune('\n')
 		}
@@ -57,9 +71,38 @@ func (c *Config) Do(method, urlpart string, body io.Reader) (*http.Response, err
 	}
 	//
 	if c.Client == nil {
-		return http.DefaultClient.Do(req)
+		return http.DefaultClient.Do(request)
 	}
 
-	fmt.Println("passou 2")
-	return c.Client.Do(req)
+	return c.Client.Do(request)
+}
+
+// getLevpayAuthenticationToken generate authentication token to execute every levpay api
+func (c *Config) getLevpayAuthenticationToken() (LevpayToken, error) {
+	var token LevpayToken
+	request, err := http.NewRequest("POST", baseURL+"/auth/", nil)
+	if err != nil {
+		return token, err
+	}
+
+	request.Header.Set("Content-type", "application/json")
+	request.Header.Set("Accept", "application/json")
+	request.SetBasicAuth(c.ApiKey, c.SecretKey)
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+
+	if err != nil {
+		return token, err
+	}
+	defer response.Body.Close()
+
+	responseBody, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return token, err
+	}
+
+	err = json.Unmarshal(responseBody, &token)
+
+	return token, nil
 }
